a83034cc3b11fd485b753d38d56150df17ccc87b
yuki <yuki@gmai.com>
2024-06-24 10:25:58 +0900

    week3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for add a review view
 
